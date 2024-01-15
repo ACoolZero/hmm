62534dd97f45d30df331480b80d1b33f31ddfe0e
ducnt2212 <ducnt2212@gmail.com>
2024-01-16 06:50:41 +0700

    fix: layout jumps when keyboard opened SCREEN_REGISTER_STEP1

--- src/screens/Auth/Register/Step1/index.tsx
+++ src/screens/Auth/Register/Step1/index.tsx
@@ -1,20 +1,19 @@
 /* eslint-disable react-native/no-inline-styles */
 import {ICONS} from '@assets';
-import {Block, FormInput, GradientButton, Image} from '@components';
+import {Block, FormContainer, FormInput, GradientButton, Image} from '@components';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {goBack, navigate} from '@navigation/NavigationServices';
 import routes from '@navigation/routes';
 import Header from '@screens/Auth/components/Header';
 import {STORE_REGISTER_DATA} from '@store/actions';
 import {getSize} from '@utils/responsive';
-import React, {useState} from 'react';
+import React from 'react';
 import {useForm} from 'react-hook-form';
 import {Pressable} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useStore} from 'react-redux';
+import {usePaddingBottom} from './helper';
 import {validation} from './validation';
-import {debounce} from 'lodash';
-import {isIos} from '@utils/helper';
 
 const INITIAL_VALUES = {fullName: '', email: ''};
 
@@ -36,56 +35,56 @@ const RegisterStep1: React.FC = () => {
     navigate(routes.REGISTER_STEP2_SCREEN);
   };
 
-  const [isTyping, setIsTyping] = useState(false);
-  const debouncedSetIsTyping = debounce(setIsTyping);
-  const _onFocus = () => debouncedSetIsTyping(true);
-  const _onBlur = () => debouncedSetIsTyping(false);
+  const {paddingBottom, onFocus, onBlur} = usePaddingBottom();
 
   return (
     <Block flex padding={24} backgroundColor="common_background">
       <Block flex paddingTop={top} justifyCenter space="between">
         <Header content="First, we want to know you" />
       </Block>
-      <Block style={{flex: !isTyping ? 2 : isIos ? 2 : 1}}>
-        <Block height={200}>
-          <FormInput
-            control={control}
-            name="fullName"
-            shadow
-            placeholder="Your name"
-            color="common_text"
-            containerInputStyle={{marginBottom: getSize.m(16)}}
-            onFocus={_onFocus}
-            onBlur={_onBlur}
-          />
-          <FormInput
-            control={control}
-            name="email"
-            shadow
-            placeholder="Email"
-            color="common_text"
-            containerInputStyle={{marginBottom: getSize.m(16)}}
-            onFocus={_onFocus}
-            onBlur={_onBlur}
-          />
+      <FormContainer>
+        <Block paddingBottom={paddingBottom}>
+          <Block height={200}>
+            <FormInput
+              control={control}
+              name="fullName"
+              shadow
+              placeholder="Your name"
+              color="common_text"
+              containerInputStyle={{marginBottom: getSize.m(16)}}
+              onFocus={() => onFocus(24)}
+              onBlur={() => onBlur()}
+            />
+            <FormInput
+              control={control}
+              name="email"
+              shadow
+              placeholder="Email"
+              color="common_text"
+              containerInputStyle={{marginBottom: getSize.m(16)}}
+              onFocus={() => onFocus(24)}
+              onBlur={() => onBlur()}
+            />
+          </Block>
+          <Block row>
+            <Pressable onPress={goBack}>
+              <Block
+                alignCenter
+                justifyCenter
+                square={50}
+                radius={8}
+                marginRight={8}
+                borderWidth={1}
+                borderColor="primary"
+                backgroundColor="white">
+                <Image source={ICONS.back} square={20} tintColor="primary" resizeMode="contain" />
+              </Block>
+            </Pressable>
+            <GradientButton isValid={isValid} title="Continue" style={{flex: 1}} onPress={handleSubmit(_onSubmit)} />
+          </Block>
         </Block>
-        <Block row>
-          <Pressable onPress={goBack}>
-            <Block
-              alignCenter
-              justifyCenter
-              square={50}
-              radius={8}
-              marginRight={8}
-              borderWidth={1}
-              borderColor="primary"
-              backgroundColor="white">
-              <Image source={ICONS.back} square={20} tintColor="primary" resizeMode="contain" />
-            </Block>
-          </Pressable>
-          <GradientButton isValid={isValid} title="Continue" style={{flex: 1}} onPress={handleSubmit(_onSubmit)} />
-        </Block>
-      </Block>
+      </FormContainer>
+      <Block flex />
     </Block>
   );
 };
