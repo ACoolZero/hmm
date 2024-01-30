d0fdabaee39a975b301fd378803f552f2d63c286
ducnt2212 <ducnt2212@gmail.com>
2024-01-31 01:40:24 +0700

    improve: open image in full screen and view detailed text

--- src/screens/Common/ActivityDetails/components/DetailLayer/styles.ts
+++ src/screens/Common/ActivityDetails/components/DetailLayer/styles.ts
@@ -1,14 +1,59 @@
-import {getSize} from '@utils/responsive';
+import {getSize, height, width} from '@utils/responsive';
 
 const styles = {
+  animatedImageContainer: ({animatedValue, STORY_WIDTH, STORY_HEIGHT, leftOffset, topOffset}: any) => ({
+    overflow: 'hidden',
+    borderRadius: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [getSize.s(24), 0],
+    }),
+    width: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [STORY_WIDTH, width],
+    }),
+    height: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [STORY_HEIGHT, height],
+    }),
+    left: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [leftOffset, 0],
+    }),
+    top: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [topOffset, 0],
+    }),
+  }),
+  animatedHeader: ({animatedValue, top}: any) => ({
+    position: 'absolute',
+    opacity: animatedValue,
+    width: width,
+    height: top + 50,
+    top: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-150, 0],
+    }),
+  }),
+  animatedDetailText: (animatedValue: any) => ({
+    position: 'absolute',
+    opacity: animatedValue,
+    bottom: animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-150, 0],
+    }),
+  }),
   closeButtonContainer: (top: number) => ({
     position: 'absolute',
-    backgroundColor: '#00000090',
     marginLeft: getSize.m(16),
+    marginTop: top,
     padding: getSize.m(12),
     borderRadius: getSize.m(6),
-    marginTop: top,
   }),
+  image: {
+    flex: 1,
+    width: 'auto',
+    height: 'auto',
+  },
 };
 
 export default styles;
